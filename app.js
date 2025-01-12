5677570bf9ab16bdf85dffb07f686d4413fe2d01
Hasan Tokatlı Edu <hasantktl.edu@gmail.com>
2025-01-12 19:48:52 +0300

    Mod çalma, durdurma, listeleme özellikleri eklendi.

---
+++ app.js
@@ -0,0 +1,78 @@
+import { modlar } from "./veri.js";
+
+function UIGoster() {
+    // state değişkenleri oluşturuluyor.
+    let premiumKullanici = false;
+    let calmaListesi = [];
+
+    const kokEleman = document.querySelector("#root");
+    const baslikElemani = document.createElement("h1");
+    const modListesiElemani = document.createElement("div");
+    modListesiElemani.id = "mod-listesi";
+    
+    baslikElemani.textContent = "ModApp v.1.0.0";
+    kokEleman.append(baslikElemani);
+    kokEleman.append(modListesiElemani);
+
+    const bodyElemani = document.querySelector("body");
+    const calmaListesiElemani = document.createElement("div");
+    calmaListesiElemani.id = "calma-listesi";
+    bodyElemani.append(calmaListesiElemani);
+    
+    modlar.forEach((mod) => {
+        // Mod resmi ve ismi için div elemanı oluşturuluyor.
+        const modElemani = document.createElement("div");
+        modElemani.className = "mod";
+        modListesiElemani.append(modElemani);
+    
+        // Mod ismi için h2 elemanı oluşturuluyor.
+        const modAdiElemani = document.createElement("h2");
+        modAdiElemani.textContent = mod.modAdi;
+        modElemani.append(modAdiElemani);
+    
+        // Mod resmi için img elemanı oluşturuluyor.
+        const modGorselElemani = document.createElement("img");
+        modGorselElemani.src = `./img/${mod.modAdi}.jpg`;
+        modElemani.append(modGorselElemani);
+    
+        // Mod sesi için audio elemanı oluşturuluyor.
+        const sesElemani = document.createElement("audio");
+        sesElemani.src = `./audio/${mod.modAdi}-sound.mp3`;
+        sesElemani.loop = true; // Sesi sürekli çalması için loop özelliği ekleniyor.
+    
+        // Mod div elemanı için tıklama olayı ekleniyor.
+        modElemani.addEventListener("click", () => {
+            if (sesElemani.paused) {
+                sesElemani.play();
+                modElemani.classList.add("aktif-mod"); // Mod div elemanına .aktif-mod css classı ekleniyor.
+                calmaListesi.push(mod.modAdi); // Çalma listesine mod adı ekleniyor.                
+            } else {
+                sesElemani.pause();
+                modElemani.classList.remove("aktif-mod"); // Mod div elemanından .aktif-mod css classı kaldırılıyor.
+                calmaListesi = calmaListesi.filter((calinanMod) => calinanMod !== mod.modAdi); // Çalma listesinden mod adı çıkarılıyor.                
+            }
+
+            calmaListesiGoster(calmaListesi);
+            // console.log(calmaListesi);
+        });
+    });
+
+}
+
+
+UIGoster();
+
+
+function calmaListesiGoster(calmaListesi) {
+    const calmaListesiElemani = document.querySelector("#calma-listesi");
+    calmaListesiElemani.innerHTML = ""; // Çalma listesi elemanının içeriği temizleniyor.
+    calmaListesi.forEach((calinanMod) => {
+        const calinanModElemani = document.createElement("p"); // Çalma listesi elemanı için p elemanı oluşturuluyor.
+        calinanModElemani.textContent = calinanMod; // Çalınan modun adı p elemanına ekleniyor.
+        calmaListesiElemani.append(calinanModElemani); // Çalma listesi elemanına p elemanı ekleniyor.
+    });
+
+    const toplamModElemani = document.createElement("p"); 
+    toplamModElemani.textContent = calmaListesi.length; 
+    calmaListesiElemani.append(toplamModElemani);
+}
---
+++ veri.js
@@ -0,0 +1,19 @@
+const modlar = [
+    { modAdi: "beach",  maksimumCalmaAdeti: 10 },
+    { modAdi: "birds",  maksimumCalmaAdeti: 2 },
+    { modAdi: "cafe",  maksimumCalmaAdeti: 6 },
+    { modAdi: "campfire",  maksimumCalmaAdeti: 4 },
+    { modAdi: "city",  maksimumCalmaAdeti: 3 },
+    { modAdi: "fireplace",  maksimumCalmaAdeti: 2 },
+    { modAdi: "forest",  maksimumCalmaAdeti: 1 },
+    { modAdi: "heavy-rain",  maksimumCalmaAdeti: 5 },
+    { modAdi: "night-crickets",  maksimumCalmaAdeti: 8 },
+    { modAdi: "rain-camping",  maksimumCalmaAdeti: 10 },
+    { modAdi: "rain",  maksimumCalmaAdeti: 9 },
+    { modAdi: "rain-windshield",  maksimumCalmaAdeti: 10 },
+    { modAdi: "snow",  maksimumCalmaAdeti: 2 },
+    { modAdi: "thunder",  maksimumCalmaAdeti: 1 },
+    { modAdi: "train",  maksimumCalmaAdeti: 7 },
+]
+
+export { modlar };
